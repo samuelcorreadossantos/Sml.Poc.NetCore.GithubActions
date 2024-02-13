513d9a6d4abe3e455252107ec01cf09a1f9d555d
Samuel Corrêa dos Santos <samuelcorreadossantos@outlook.com>
2024-02-13 15:21:31 -0400

    Add project files.

---
+++ Sml.Poc.NetCore.GithubActions.Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sml.Poc.NetCore.GithubActions.Api.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("Funcionou :)");
+        }
+    }
+}
